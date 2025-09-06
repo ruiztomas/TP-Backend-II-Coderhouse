2b5fbb66d45b060d1af14f7df14bcb8a7b0ac07f
ruiztomas <tomialeruiz@gmail.com>
2025-09-06 00:48:23 -0300

    ..

--- src/controllers/sessions.controller.js
+++ src/controllers/sessions.controller.js
@@ -1,69 +1,37 @@
-import { usersService } from "../services/index.js";
-import { createHash, passwordValidation } from "../utils/index.js";
-import jwt from 'jsonwebtoken';
-import UserDTO from '../dto/User.dto.js';
+import jwt from "jsonwebtoken";
+import UserDTO from "../dto/User.dto.js";
+import UserDTO from "../dto/User.dto.js";
 
-const register = async (req, res) => {
-    try {
-        const { first_name, last_name, email, password } = req.body;
-        if (!first_name || !last_name || !email || !password) return res.status(400).send({ status: "error", error: "Incomplete values" });
-        const exists = await usersService.getUserByEmail(email);
-        if (exists) return res.status(400).send({ status: "error", error: "User already exists" });
-        const hashedPassword = await createHash(password);
-        const user = {
-            first_name,
-            last_name,
-            email,
-            password: hashedPassword
-        }
-        let result = await usersService.create(user);
-        console.log(result);
-        res.send({ status: "success", payload: result._id });
-    } catch (error) {
-
+const register=async(req,res)=>{
+    try{
+        res.send({status: "sucess", message: "User registered"});
+    }catch(error){
+        console.error("Error en register", error);
+        res.status(500).send({status: "error", error: "Register failed"});
     }
-}
+};
 
-const login = async (req, res) => {
-    const { email, password } = req.body;
-    if (!email || !password) return res.status(400).send({ status: "error", error: "Incomplete values" });
-    const user = await usersService.getUserByEmail(email);
-    if(!user) return res.status(404).send({status:"error",error:"User doesn't exist"});
-    const isValidPassword = await passwordValidation(user,password);
-    if(!isValidPassword) return res.status(400).send({status:"error",error:"Incorrect password"});
-    const userDto = UserDTO.getUserTokenFrom(user);
-    const token = jwt.sign(userDto,'tokenSecretJWT',{expiresIn:"1h"});
-    res.cookie('coderCookie',token,{maxAge:3600000}).send({status:"success",message:"Logged in"})
-}
+const login=async(req,res)=>{
+    try{
+        if(req.user){
+            return res.status(401).send({status:"error", error: "Invalid credentials"});
+        }
+        const UserDTO=UserDTO.getUserTokenFrom(req.user);
+        const token=jwt.sign(UserDTO, "tokenSecretJWT",{ expiresIn: "1h"});
+        res
+            .cookie("coderCookie", token, {maxAge: 3600000, httpOnly: true})
+            .send({status: "success", message:"Logged in"});
+    }catch(error){
+        console.error("Error en login:", error);
+        res.status(500).send({status:"error", error: "Login gailed"});
+    }
+};
 
-const current = async(req,res) =>{
-    const cookie = req.cookies['coderCookie']
-    const user = jwt.verify(cookie,'tokenSecretJWT');
-    if(user)
-        return res.send({status:"success",payload:user})
-}
-
-const unprotectedLogin  = async(req,res) =>{
-    const { email, password } = req.body;
-    if (!email || !password) return res.status(400).send({ status: "error", error: "Incomplete values" });
-    const user = await usersService.getUserByEmail(email);
-    if(!user) return res.status(404).send({status:"error",error:"User doesn't exist"});
-    const isValidPassword = await passwordValidation(user,password);
-    if(!isValidPassword) return res.status(400).send({status:"error",error:"Incorrect password"});
-    const token = jwt.sign(user,'tokenSecretJWT',{expiresIn:"1h"});
-    res.cookie('unprotectedCookie',token,{maxAge:3600000}).send({status:"success",message:"Unprotected Logged in"})
-}
-const unprotectedCurrent = async(req,res)=>{
-    const cookie = req.cookies['unprotectedCookie']
-    const user = jwt.verify(cookie,'tokenSecretJWT');
-    if(user)
-        return res.send({status:"success",payload:user})
-}
-export default {
-    current,
-    login,
-    register,
-    current,
-    unprotectedLogin,
-    unprotectedCurrent
+const current=asyn(req,res)=>{
+    try{
+        if(!req.user){
+            return res.status(401).send({status: "error", error: "Not authenticated"});
+            
+        }
+    }
 }
